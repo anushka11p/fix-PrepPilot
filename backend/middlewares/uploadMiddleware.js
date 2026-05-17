a38fa94aec4af274aa4868ae0e5a603a156f7cbb
Boes <ismael.a@live.nl>
2026-05-17 20:26:31 +0200

    Add upload file size limit

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,5 +1,7 @@
 const multer = require("multer");
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+
 // Configure storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -21,6 +23,12 @@ const fileFilter = (req, file, cb) => {
 };
 
 // Initialize upload
-const upload = multer({ storage, fileFilter });
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+  },
+});
 
 module.exports = upload;
